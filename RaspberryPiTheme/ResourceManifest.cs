1992e61a1b729fa9c79e83a7dbb4cbbcce5b6991
senshi111 <40372242+Senshi111@users.noreply.github.com>
2021-01-02 18:52:36 +0100

    add mdb 5

--- RaspberryPiTheme/ResourceManifest.cs
+++ RaspberryPiTheme/ResourceManifest.cs
@@ -13,6 +13,38 @@ namespace OrchardCore.Themes.RaspberryPiTheme
                 .SetUrl("~/RaspberryPiTheme/css/bootstrap-oc.min.css", "~/RaspberryPiTheme/css/bootstrap-oc.css")
                 .SetVersion("1.0.0");
 
+
+
+            manifest
+                .DefineStyle("RaspberryPiTheme-css" )
+                .SetUrl("~/RaspberryPiTheme/css/mdb.min.css")
+                .SetVersion("1.0.0");
+
+
+
+
+            manifest
+                .DefineScript("RaspberryPiTheme-js")
+                .SetUrl("~/RaspberryPiTheme/js/mdb.min.js")
+                .SetVersion("1.0.0");
+
+
+
+
+            manifest
+                .DefineStyle("RaspberryPiTheme-vendor-font-awesome")
+                .SetUrl("https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.14.0/css/all.min.css")
+                .SetCdnIntegrity("sha256-FMvZuGapsJLjouA6k7Eo2lusoAX9i0ShlWFG6qt7SLc=")
+                .SetVersion("5.14.0");
+
+
+
+
+            manifest
+                .DefineScript("RaspberryPiTheme-vendor-font-awesome")
+                .SetCdn("https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.14.0/js/fontawesome.min.js")
+                .SetCdnIntegrity("sha256-dNZKI9qQEpJG03MLdR2Rg9Dva1o+50fN3zmlDP+3I+Y=")
+                .SetVersion("5.14.0");
         }
     }
 }
